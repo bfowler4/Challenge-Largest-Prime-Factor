379c9215c57e38cf135e88b8ad64b7289a183a5e
Brandon Fowler <bmfowler4@gmail.com>
2018-01-24 09:44:39 -1000

    finished brute force method

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,23 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+  let nSquare = Math.ceil(Math.sqrt(n));
+  for (let i = nSquare; i >= 0; i --) {
+    if (n % i === 0) {
+      if (isPrime(i)) {
+        return i;
+      }
+    }
+  }
 
   return primeNumber;
 };
+
+function isPrime(num) {
+  let numSquare = Math.ceil(Math.sqrt(num));
+  for (let i = 2; i < numSquare; i ++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
